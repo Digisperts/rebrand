90577bbc79ac59e171ea96b310f23bdae347ca00
oluphunmmy <olufunmilayoagboola@gmail.com>
2025-08-25 17:13:44 +0100

    education case study card added

--- rebrand/src/app/resources/case-studies/page.tsx
+++ rebrand/src/app/resources/case-studies/page.tsx
@@ -416,10 +416,10 @@ export default function CasesStudiesResource() {
                 accessible knowledge for all.
               </p>
               <Link
-                href="/images/case-studies/educationcard.pdf"
+                href="/images/case-studies/LMS-case-study.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
-                onClick={(e) => e.preventDefault()}
+                // onClick={(e) => e.preventDefault()}
                 className="border bg-[#003399] border-gray-800 px-4 py-2 text-white font-bold text-sm hover:bg-white hover:text-[#233876] transition-colors duration-200 inline-block shadow"
                 style={{ boxShadow: "4px 4px 4px 0px #00000040" }}
               >
